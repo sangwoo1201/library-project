8b2a4a4e40d2feddcea10b49a7e41c1e5542850d
sangwoo1201 <lsw1096@naver.com>
2022-11-22 14:35:32 +0900

    (Refactor) : DurationVO 생성

---
+++ library/src/main/java/com/example/library/vo/DurationVO.java
@@ -0,0 +1,42 @@
+package com.example.library.vo;
+
+public class DurationVO {
+
+	private int durationNo;
+	private String startAt;
+	private String endAt;
+	private String id;
+	private String uniqueNumber;
+	
+	public int getDurationNo() {
+		return durationNo;
+	}
+	public void setDurationNo(int durationNo) {
+		this.durationNo = durationNo;
+	}
+	public String getStartAt() {
+		return startAt;
+	}
+	public void setStartAt(String startAt) {
+		this.startAt = startAt;
+	}
+	public String getEndAt() {
+		return endAt;
+	}
+	public void setEndAt(String endAt) {
+		this.endAt = endAt;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUniqueNumber() {
+		return uniqueNumber;
+	}
+	public void setUniqueNumber(String uniqueNumber) {
+		this.uniqueNumber = uniqueNumber;
+	}
+	
+}
