04e5102c13f30216a47f04756b4e76da3fa8d272
sangwoo1201 <lsw1096@naver.com>
2022-11-23 14:05:07 +0900

    (Refactor) : 책 전체 리스트 컨트롤러 추가

--- library/src/main/java/com/example/library/controller/MainController.java
+++ library/src/main/java/com/example/library/controller/MainController.java
@@ -1,8 +1,22 @@
 package com.example.library.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.library.service.MainService;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private MainService service;
+	
+	@GetMapping("/books/all")
+	public List<Map<String, Object>> callAllBooks(){
+		return service.getAllBooks();
+	}
 }
