47bc0584bbcac9c1dace2cb06584fdaa89db2165
sangwoo1201 <lsw1096@naver.com>
2022-11-23 13:55:28 +0900

    (Refactor) : 책 전체 리스트 매퍼 추가

--- library/src/main/java/com/example/library/mapper/MainMapper.java
+++ library/src/main/java/com/example/library/mapper/MainMapper.java
@@ -1,8 +1,15 @@
 package com.example.library.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.library.vo.BookVO;
 
 @Mapper
 public interface MainMapper {
+	
+	public List<Map<String, Object>> selectAllBooks();
 	
 }
